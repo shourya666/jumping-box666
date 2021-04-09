682f3465d85b5e869497e8ce3174a7e97818e2a7
shourya666 <77875199+shourya666@users.noreply.github.com>
2021-04-09 14:24:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var canvas;
 var music;
 var box ;
 var surface1,surface2,surface3,surface4;
-box.velocityY = 4;
+
 
 function preload(){
     music = loadSound("music.mp3");
@@ -12,22 +12,51 @@ function preload(){
 function setup(){
     canvas = createCanvas(800,600);
     box = createSprite(200,50,20,20)
-    //create 4 different surfaces
-     surface1 = createSprite(200,30,50,20)
-     surface2 = createSprite(200,30,50,20)
-     surface3 = createSprite(200,30,50,20)
-     surface4 = createSprite(200,30,50,20)
-    
 
-    //create box sprite and give velocity
+     surface1 = createSprite(0,580,360,30)
+     surface1. shapeColor = rgb(0,0,255)
+     surface2 = createSprite(295,580,200,30)
+     surface2. shapeColor = rgb(255,128,0)
+     surface3 = createSprite(515,580,200,30)
+     surface3.shapeColor = rgb(153,0,76)
+     surface4 = createSprite(740,580,220,30);
+     surface4.shapeColor = rgb(0,100,0);
+
+    box= createSprite(random(20,750),100, 40,40);
+    box.shapeColor = rgb(255,255,255);
+    box.velocityX = 5;
+    box.velocityY = 7;
 
 }
-
 function draw() {
-    background(rgb(169,169,169));
-    createEdgeSprites();
 
 
 
-    //add condition to check if box touching surface and make it box
+background(rgb(169,169,169));
+edges=createEdgeSprites();
+box.bounceOff(edges);
+
+if(surface1.isTouching(box) && box.bounceOff(surface1)){
+    box.shapeColor = rgb(0,0,255);
+    music.play();
 }
+
+if(surface22.isTouching(surface2)){
+    box.shapeColor = rgb(255,128,0);
+   box .velocityX = 0;
+    box.velocityY = 0;
+    music.stop();
+}
+
+if(surface3.isTouching(box) && box.bounceOff(surface3)){
+    box.shapeColor = rgb(153,0,76);
+}
+
+if(surace4.isTouching(box) && box.bounceOff(surface4)){
+    box.shapeColor = rgb(0,100,0);
+}
+
+drawSprites();
+}
+
+
